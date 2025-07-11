d3f42de3edc3a831f6bb29fc87df1df058cee652
Vbdulhvm33d <abdulhameedopeyemi41@gmail.com>
2025-07-11 12:14:09 +0100

    Add my first python file

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+#variable definition
+number1 = 10
+number2 = 5
+#performing basic arithmetic operations
+sum_of = number1 + number2
+difference_of = number1 - number2
+product_of = number1 * number2
+#output
+print("addition of", number1, "and", number2, "is", sum_of)
+print("subtraction of", number1, "and", number2, "is", difference_of)
+print("multiplication of", number1, "and", number2, "is", product_of)
