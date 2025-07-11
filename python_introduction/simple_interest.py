5cc1cae92b02dfff28597a914d431fa7b6c7819e
Vbdulhvm33d <abdulhameedopeyemi41@gmail.com>
2025-07-11 18:41:11 +0100

    Add simple_interest.py

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,8 @@
+#Define varialbles for a simple interest calculator
+principal = 1000
+rate = 0.05
+time = 3
+#Define simple interest
+interest = principal * rate * time
+#calculation of simple interest
+print("The simple interest is", interest)
