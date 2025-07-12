2d07924d802e3803fc9c058242e9a82c913c76e1
Vbdulhvm33d <abdulhameedopeyemi41@gmail.com>
2025-07-12 11:21:12 +0100

    Add debugged the hour_to_second bug

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,5 +1,6 @@
 #define variables for hour to seconds conversion
 hours = 2
-seconds = 3600 * hours
+conversion_rate = 3600 
+seconds = hours * conversion_rate
 #calculate the hour to second equivalent
-print(hours, "hours", "is", seconds "seconds")
+print(hours, "hours is", seconds, "seconds")
