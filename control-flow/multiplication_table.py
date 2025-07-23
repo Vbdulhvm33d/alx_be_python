1461817f776b87064fd16f12a306469053f894e3
Vbdulhvm33d <abdulhameedopeyemi41@gmail.com>
2025-07-23 17:38:15 +0100

    multip_tab_update

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,10 +1,10 @@
 #taking user inputs
 number = int(input("Enter a number to see its multiplication table: "))
-i = number
+X = number
 #defining the range of numbers usin the for loop
-for j in range (1, 11):
-    k = i * j
-    print(f"{i} * {j} = {k}")
+for Y in range (1, 11):
+    Z = X * Y
+    print(f"{X} * {Y} = {Z}")
 print()
 
     
