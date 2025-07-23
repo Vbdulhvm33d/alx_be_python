8c0c32f1e8986ce46cd9f058b728b405dca5faa5
Vbdulhvm33d <abdulhameedopeyemi41@gmail.com>
2025-07-23 15:26:46 +0100

    output message add

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -7,17 +7,17 @@ operation = input("Choose the operation (+, -, *, /): ")
 match operation:
     case "+" :
         result = num1 + num2
-        print(f"the sum of {num1} and {num2} is {result}")
+        print(f"the result is {result}")
     case "-" :
         result = num1 - num2
-        print(f"the difference between {num1} and {num2} is {result}")
+        print(f"the result is {result}")
     case "*" :
         result = num1 * num2
-        print(f"the product of {num1} and {num2} is {result}")
+        print(f"the result is {result}")
     case "/" :
         result = num1 / num2
         if num2 != 0:
-            print(f"the division of {num1} and {num2} is {result}")
+            print(f"the result is {result}")
         else:
             print("cannot divide by zero")
     case _ :
