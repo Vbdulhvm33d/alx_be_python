ab982fcbc4f92823129aef1d6db7400befb8fbd8
Vbdulhvm33d <abdulhameedopeyemi41@gmail.com>
2025-07-23 13:21:28 +0100

    updated weather advice

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -4,11 +4,11 @@ rainy = "Don't forget your umbrella and a raincoat."
 cold = "Make sure to wear a warm coat and a scarf."
 #prompting users for input
 weather = input("What's the weather like today? (sunny/rainy/cold):")
-if weather.lower() == "sunny":
+if sunny:
     print(f"{sunny}")
-elif weather.lower() == "rainy":
+elif rainy:
     print(f"{rainy}")
-elif weather.lower() == "cold":
+elif cold:
     print(f"{cold}")
 else:
     print("Sorry, I don't have recommendations for this weather.")
