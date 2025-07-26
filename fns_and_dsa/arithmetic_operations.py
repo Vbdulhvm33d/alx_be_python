5ab7cf8da5730d8e8271194b96f4224e5158d602
Vbdulhvm33d <abdulhameedopeyemi41@gmail.com>
2025-07-26 18:11:40 +0100

    add arithmetic_operations.py

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,23 @@
+#defining arithmetic functions
+def perform_operation(num1, num2, operation):
+    num1 = float(input("Enter the first number:"))
+    num2 = float(input("Enter the second number:"))
+    operation = input("Enter the operation (add, subtract, multiply, divide)").strip().lower()
+    #individual operation definition
+    if operation == add:
+        add = num1 + num2
+        print(f"result: {add}")
+    elif operation == subtract:
+        subtract = num1 - num2
+        print(f"result: {subtract}")
+    elif operation == multiply:
+        multiply = num1 * num2
+        print(f"result: {multiply}")
+    elif operation == divide:
+        divide = num1 / num2
+        if num2 == 0:
+            print(f"error encountered,change your dividend")
+        else:
+            print(f"result: {divide}")
+
+   
