b8370525c0c025c11ad36c89b65538c155c5bb02
Vbdulhvm33d <abdulhameedopeyemi41@gmail.com>
2025-07-27 22:24:53 +0100

    add data

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,43 @@
+#defining display menu
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove_Item")
+    print("3. View List") 
+    print("4. Exit") 
+
+def main():
+     shopping_list =[]
+
+while True :
+        display_menu()
+        choice = input("enter your choice: ")
+        shopping_list = []
+        
+
+        if choice == "1":
+                updated_list = shopping_list.append(input("enter item to include: "))
+                pass
+                
+        
+        elif choice == "2":
+            updated_list = shopping_list.remove(input("enter item to be removed: "))
+            pass
+
+        elif choice == "3":
+                for items in shopping_list:
+                        print(items)
+                        pass
+
+        elif choice == 4:
+                print("Goodbye!")
+                break
+
+        else:
+                print("Invalid choice. Please try again. ")
+
+if __name__ == "__main__":
+    main()
+
+
+        
