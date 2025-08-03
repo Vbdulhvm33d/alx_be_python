8c65cff12e5bd8bc19aa5a9b01effcbd3f04c453
Vbdulhvm33d <abdulhameedopeyemi41@gmail.com>
2025-08-03 22:12:01 +0100

    add display_balance updates

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -2,32 +2,32 @@
 
 class BankAccount:
 
-    def __init__(self, initial_balance=0):
-        self.__account_balance = initial_balance
+    def __init__(self, account_balance=0):
+        self.account_balance = account_balance
 
     def deposit(self, amount):
         #this line houses the code 
         if amount > 0:
-            self.__account_balance += amount
+            self.account_balance += amount
             print(f"you've successfully deposited ${amount}")
         else:
             print("please enter a positive amount")
 
     def withdraw(self, amount):
         #this code houses the code
-        if amount <= self.__account_balance:
-            self.__account_balance -= amount
+        if amount <= self.account_balance:
+            self.account_balance -= amount
             print(f"withdrawal of ${amount} successful.")
             return True
         
-        elif amount > self.__account_balance:
+        elif amount > self.account_balance:
             print(f"insufficient funds!")
             return False
              
     def display_balance(self):
-        print(f"current balance: ${self.__account_balance}")
+        print(f"current balance: ${self.account_balance}")
 
 my_acct = BankAccount(0)
-my_acct.deposit(100)
-#my_acct.withdraw(49)
+#my_acct.deposit(100)
+my_acct.withdraw(49)
 my_acct.display_balance()
